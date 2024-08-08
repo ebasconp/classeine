5702711394c98a64e16a2df7f672a8163cbc1206
Ernesto Alfonso Bascón Pantoja <ebasconp@gmail.com>
2024-08-08 14:04:05 +0200

    CLA-13: added Strings::format(), Console::print(), etc.

--- code/classeine-lib/clsn/core/Panic.h
+++ code/classeine-lib/clsn/core/Panic.h
@@ -1,8 +1,20 @@
 #pragma once
 
-#include <string>
+#include "Console.h"
 
 namespace clsn::core
 {
-    void Panic(const std::string& msg, int errorCode = -1);
+    template <typename... Args>
+    void Panic(int exitCode, std::string_view fmtspec, const Args&... args)
+    {
+        Console::print_stream(std::cerr, "Runtime error: ");
+        Console::println_stream(std::cerr, fmtspec, args...);
+        exit(exitCode);
+    }
+
+    template <typename... Args>
+    void Panic(std::string_view fmtspec, const Args&... args)
+    {
+        Panic(-1, fmtspec, args...);
+    }
 }
