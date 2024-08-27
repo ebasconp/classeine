29b3cf241977b4e66bc563b6a599b976c3e7c078
Ernesto Alfonso Bascón Pantoja <ebasconp@gmail.com>
2024-08-28 00:37:56 +0200

    cla-31: simplify layout container renderers

--- code/classeine-lib/clsn/ui/hbox_layout_container.cpp
+++ code/classeine-lib/clsn/ui/hbox_layout_container.cpp
@@ -1,6 +1,6 @@
 #include "hbox_layout_container.h"
 
-#include "renderers/hbox_layout_container_renderer.h"
+#include "renderers/container_renderer.h"
 
 namespace clsn::ui
 {
@@ -11,6 +11,6 @@ namespace clsn::ui
 
     auto hbox_layout_container::make_default_renderer() const -> std::unique_ptr<renderer_base>
     {
-        return std::make_unique<renderers::hbox_layout_container_renderer>();
+        return std::make_unique<renderers::container_renderer<hbox_layout_container>>();
     }
 }
--- code/classeine-lib/clsn/ui/renderers/hbox_layout_container_renderer.cpp
+++
@@ -1,5 +0,0 @@
-#include "hbox_layout_container_renderer.h"
-
-namespace clsn::ui::renderers
-{
-}
